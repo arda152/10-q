57f9e89890d55818d820c7ef102e49fad745c601
Arda Arman <ardaarman99@gmail.com>
2018-09-06 17:31:33 +0200

    Quiz now playable Added restart button Showing progress

--- javascript.js
+++ javascript.js
@@ -17,8 +17,30 @@ let quizSetup = document.getElementById("quizSetup");
 let quizDisplay = document.getElementsByClassName("quizDisplay");
 let progressDisplay = document.getElementById("progressDisplay");
 let answersDisplay = document.getElementById("answersDisplay");
+let endDisplay = document.getElementById("endDisplay");
+let resultsDisplay = document.getElementById("resultsDisplay");
+let restartButtonDiv = document.getElementById("restartButtonDiv");
 
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", quizInit);
+function quizRestart() {
+    // Cleanup
+    progressDisplay.textContent = "";
+    resultsDisplay.textContent = "";
+    restartButtonDiv.textContent = "";
+    quizSetup.classList.remove("hide");
+}
+function endQuiz(points) {
+    questionDisplay.textContent = "";
+    answersDisplay.textContent = "";
+    resultsDisplay.textContent = "You answered " + points + " question" + (points !== 1 ? "s" : "") + " correctly!";
+    let restartButton = document.createElement("a");
+    restartButton.className = "button";
+    restartButton.textContent = "Restart";
+    restartButton.addEventListener("click", quizRestart);
+    restartButtonDiv.appendChild(restartButton);
+}
+
+function quizInit() {
     // Assign values
     let selectedCategory = document.getElementById("categorySelect").value;
     let selectedDifficulty = document.getElementById("difficultySelect").value;
@@ -44,7 +66,7 @@ startButton.addEventListener("click", function() {
     }, function(err) {
         console.log("error: " + err);
     });
-});
+}
 
 // Run the quiz with returned results from database
 function showQuestion(quizState) {
@@ -70,10 +92,17 @@ function handleAnswerClick(quizState) {
     questionDisplay.textContent = "";
     console.log("clicked on answer " + this.id);
     // Check the answer, show the correct answer
+    let currentCorrectAnswer = quizState.questionData[quizState.counter].correct_answer;
+    if (currentCorrectAnswer === this.textContent) {
+        addMark(true);
+        quizState.points++;
+    } else {
+        addMark(false);
+    }
     // ...
     // If the game is over
-    if (quizState.counter === 9) {
-        console.log("end of quiz");
+    if (quizState.counter === 4) {
+        endQuiz(quizState.points);
     } else {
     // Show the next question
     quizState.counter++;
@@ -81,6 +110,20 @@ function handleAnswerClick(quizState) {
     }
 }
 
+
+// Add the correct mark for an answer
+function addMark(answerIsCorrect) {
+    // Create and initialize icon
+    let iconContainer = document.createElement("span");
+    iconContainer.className = "icon";
+    let icon = document.createElement("i");
+    icon.className = "fas";
+    iconContainer.appendChild(icon);
+    // Select the right icon
+    icon.classList.add(answerIsCorrect ? "fa-check" : "fa-times");
+    // Add icon
+    progressDisplay.appendChild(iconContainer);
+}
 // Create the request URL u sing category name and difficulty level
 function createRequestURL(categoryValue, difficultyValue) {
     // Default setup
